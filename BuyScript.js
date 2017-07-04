709754a985f654ab924670cbaf39cc505e719dea
Melanie Smith <mels59@gmail.com>
2017-07-04 12:28:35 -0400

    added sell your house functionality
    changed mitchell to under contract

--- BuyScript.js
+++ BuyScript.js
@@ -13,7 +13,7 @@ function Property(name, picture, bed, bath, size, price, link) {
 
 
 var Mitchell = new Property("Mitchell Creek", "images/MitchellCreek.jpg", "3", "2",
-    "1188", "$167,000", "Mitchell.html");
+    "1188", "Under Contract", "Mitchell.html");
 var OliverCrest = new Property("Oliver Crest", "images/oliverCrest.jpg", "3", "1.5",
     "1040", "Coming Soon", "#");
 var Warren = new Property("Warren Road", "images/WarrenRd.jpg", "3", "2", "1092", "Coming Soon", "#");
@@ -29,7 +29,7 @@ var card5 = ["propertyName5", "specs5", "picture5"];
 var card6 = ["propertyName6", "specs6", "picture6"];
 
 
-var houseArray = [Mitchell, OliverCrest, Warren, Newton, Tanglewood, MountainRidge];
+var houseArray = [Mitchell, Warren, OliverCrest, Newton, Tanglewood, MountainRidge];
 var cardsArray = [card1, card2, card3, card4, card5, card6];
 var soldArray = [Newton, Tanglewood, MountainRidge];
 
@@ -69,6 +69,8 @@ function getColor(price) {
         color = "orange";
     } else if (price == "Coming Soon") {
         color = "olive";
+    }else if(price=="Under Contract"){
+        color = "yellow";
     }
     return color;
 }
